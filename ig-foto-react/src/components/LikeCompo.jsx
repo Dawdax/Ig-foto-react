d62f4249dd17e3d68e6501785f6d9fb537ba8204
unknown <davidaguilar5034@gmail.com>
2024-06-21 20:53:49 -0600

    Aplicación terminada

--- ig-foto-react/src/components/LikeCompo.jsx
+++ ig-foto-react/src/components/LikeCompo.jsx
@@ -1,16 +1,47 @@
-import React from 'react';
+import React, { useState } from 'react';
 
-export const LikeCompo = () => {
+
+const estadoInicial = {
+  like: false,
+  likeContador: 243,
+  save: false
+}
+
+export const LikeCompo = () =>   
+  {
+  const [estado, setEstado] = useState(estadoInicial)
+  
+  const cambiandoEstadoLike = () =>{
+    setEstado(prevState => ({
+      ...prevState,
+      like: !prevState.like,
+      likeContador: prevState.like === false ? prevState.likeContador + 1 : prevState.likeContador - 1
+    }))
+  }
+
+  const cambiandoEstadoSave = () =>{
+      setEstado(prevState => ({
+        ...prevState,
+        save: !prevState.save,
+      }))
+   }
+
+  const likeState = estado.like === false ? "like-off" 
+                                          : "like-on"
+  
+  const saveState = estado.save === false ? 'save-off' 
+                                          : 'save-on'
+
   return (
     <>
       <div className="grid grid-cols-8 gap-4 py-[1rem]">
-        <div className="like-off col-span-1"></div>
+        <div onClick={cambiandoEstadoLike} className={`${likeState} col-span-1`}  ></div>
         <div className="comentario col-span-1"></div>
         <div className="compartir col-span-1"></div>
-        <div className="col-end-10 p-0 m-0 save-off"></div> {/* Último elemento comienza en la columna 8 */}
+        <div onClick={cambiandoEstadoSave} className={`${saveState} col-end-10 p-0 m-0`}></div> {/* Último elemento comienza en la columna 8 */}
       </div>
       <div className=' text-xl'>
-        <p>234 likes</p>
+        <p>{estado.likeContador} Likes</p>
         <p><b>Dawdax</b> Estoy usando ReactJs con useState :D </p>
       </div>
     </>
